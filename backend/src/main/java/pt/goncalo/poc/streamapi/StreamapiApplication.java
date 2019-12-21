feb73f7c55da896cccf4c71abacb029581bead02
Gonçalo Silva <goncalosamuel@gmail.com>
2019-12-21 11:36:28 +0000

    working with rabbitMQ

--- backend/src/main/java/pt/goncalo/poc/streamapi/StreamapiApplication.java
+++ backend/src/main/java/pt/goncalo/poc/streamapi/StreamapiApplication.java
@@ -2,17 +2,15 @@ package pt.goncalo.poc.streamapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.integration.annotation.MessageEndpoint;
 
 @SpringBootApplication
-@EnableBinding({Source.class, Sink.class})
-@MessageEndpoint
+
 public class StreamapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamapiApplication.class, args);
 	}
+
+
+
 }
